142ffd22f25522bd3bbb27bf1c367b3540af5aa5
mohsen <moh3en0731@gmail.com>
2017-11-19 20:42:20 +0330

    mohsen first Commit

---
+++ WebApplication-git-test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_git_test.MvcApplication" Language="C#" %>
---
+++ WebApplication-git-test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication_git_test.Startup))]
+namespace WebApplication_git_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
